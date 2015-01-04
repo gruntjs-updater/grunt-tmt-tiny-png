b4469b839d6dc2763f7f36551c3cf774054ee5d5
funfungo <funfungo@foxmail.com>
2015-01-04 20:10:52 +0800

    up

--- tasks/tmtTinyPng.js
+++ tasks/tmtTinyPng.js
@@ -6,34 +6,33 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-var request = require('request');
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
-var KEY = ['DCW-nZpuisKWg5iZV8rH0k335X7z_Bjb','eEamUCpDZYQ7zaj-frcAycqaKsB4Bou5','pVOpSrHyzPyTLqWrqkRJHAptvThQI9VU'];
+"use strict";
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
+var async = require("async");
+var KEY = ["DCW-nZpuisKWg5iZV8rH0k335X7z_Bjb","eEamUCpDZYQ7zaj-frcAycqaKsB4Bou5","3zdVSHVOGk6xnn71-3JfOh1-l-BROnKQ",
+    "pVOpSrHyzPyTLqWrqkRJHAptvThQI9VU","FpzjI6viK9Q1hhFoZqXmpMzNj6VFkYMO","2lADPJQnfA3g8R1n6ObMuedbWh2ggxNq","pRLOxoMPpkLbFyAly7sRGoIWeVcQWt_i","uIS21F7vsXXZhJ599Wb3MsriVxgt5JYz"];
 
 module.exports = function (grunt) {
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
 
-    grunt.registerMultiTask('tmtTinyPng', 'The best Grunt plugin ever.', function () {
-
+    grunt.registerMultiTask("tmtTinyPng", "The best Grunt plugin ever.", function () {
         var done = this.async();
         var timeData = {};
-        var keyarr = fs.readFileSync('key.txt','utf-8').split(",");
+        var keyarr = fs.readFileSync("key.txt","utf-8").split(",");
         var apiKey = getKey();
-        console.log(apiKey);
-        var index;
+        var keyIndex;
         var startTime,endTime,localStart = {};
         var inputSize = 0,outputSize = 0;
         var pressMessage={};
         var options = {
-            uri: 'https://api.tinypng.com/shrink',
-            method: 'POST',
+            uri: "https://api.tinypng.com/shrink",
+            method: "POST",
             auth: {
-                user: 'api',
+                user: "api",
                 pass: apiKey
             },
             timeout: 15000
@@ -46,22 +45,25 @@ module.exports = function (grunt) {
         //Get the Api key from tinypng.com
         //See https://tinypng.com/developers
         function getKey(){
-            index = Math.floor(Math.random()*keyarr.length);
-            return keyarr[index];
+            keyIndex = Math.floor(Math.random()*keyarr.length);
+            console.log(keyarr[keyIndex]);
+            return keyarr[keyIndex];
         }
         //Romove the useless api key from key file and draw a new key from it
-        function removeBadKey(index){
-            keyarr.splice(index,1);
+        function removeBadKey(keyIndex){
+            if(keyarr.indexOf(apiKey) != -1){
+                keyarr.splice(keyIndex,1);
+            }
             apiKey = getKey();
             options.auth.pass = apiKey;
-            fs.writeFileSync('key.txt',keyarr.join(','),'utf-8');
+            fs.writeFileSync("key.txt",keyarr.join(","),"utf-8");
         }
         //reset the key file in the beginning of every month
         function resetKeyFile(){
             startTime = new Date();
             var date = startTime.getDate();
-            if(date === 2){
-                fs.writeFileSync('key.txt',KEY.join(','),'utf-8');
+            if(date >= 2 && date < 10){
+                fs.writeFileSync("key.txt",KEY.join(","),"utf-8");
             }
         }
         //
@@ -78,17 +80,16 @@ module.exports = function (grunt) {
                     console.log(key);
                     if(key !== inputSize && key !== outputSize && key != ratio && key !== "total time");
                     {
-                        writeFile.write(key+"\t"+data[key].inputSize+"\t"+data[key].outputSize+'\t'+data[key].uploadTime+"\t"+data[key].downloadTime+"\n");
+                        writeFile.write(key+"\t"+data[key].inputSize+"\t"+data[key].outputSize+"\t"+data[key].uploadTime+"\t"+data[key].downloadTime+"\n");
                     }
                 }
             });
-
         }
 
 
         var q = async.queue(function(file,callback){
             (function upload(file){
-                localStart[index] = new Date();
+                localStart[file] = new Date();
                 grunt.log.ok("uploading..." + file);
                 timeData[file].uploadCount ++;
                 fs.createReadStream(file).pipe(request(options,function(error,response){
@@ -97,33 +98,36 @@ module.exports = function (grunt) {
                             pressMessage = JSON.parse(response.body);
                             inputSize += pressMessage.input.size;
                             outputSize +=  pressMessage.output.size;
-                            timeData[file].uploadTime = getTime(localStart[index]);
+                            timeData[file].uploadTime = getTime(localStart[file]);
                             timeData[file].inputSize = pressMessage.input.size;
                             timeData[file].outputSize = pressMessage.output.size;
                             grunt.log.ok(file + " compressed OK");
                             (function getPng(file){
-                                localStart[index] = new Date();
-                                request({uri:response.headers.location,timeout:20000})
-                                    .on('error',function(error){
+                                localStart[file] = new Date();
+                                console.log(response.headers.location);
+                                request({uri:response.headers.location,timeout:2000,Pool:false})
+                                    .on("error",function(error){
                                         grunt.log.error("downloading ERROR " + error.message);
                                         getPng(file);
                                     })
-                                    .on('end',function(){
-                                        timeData[file].downloadTime = getTime(localStart[index]);
+                                    .on("end",function(){
+                                        timeData[file].downloadTime = getTime(localStart[file]);
                                         grunt.log.ok(file + " downloaded");
                                         callback();
                                     })
                                     .pipe(fs.createWriteStream(file));
                             })(file);
                         }else if(response.statusCode == 429){
-                            removeBadKey(index);
+                            removeBadKey(keyIndex);
                             console.log(apiKey);
                             grunt.log.error("key expires");
                             q.push(file);
                             callback();
                         }
                         else{
-                            q.push(file);
+                            if(timeData[file].uploadCount <= 5) {
+                                q.push(file);
+                            }
                             grunt.log.error("error" + response.statusCode);
                             callback();
                         }
@@ -137,11 +141,8 @@ module.exports = function (grunt) {
                             callback();
                         }
                     }
-
                 }));
-
             })(file);
-
         },200);
 
         q.drain = function(){
@@ -156,7 +157,6 @@ module.exports = function (grunt) {
 
         //begin
         resetKeyFile();
-
         this.files.forEach(function(f){
             f.src.forEach(function(file){
                 timeData[file] = {};
@@ -166,6 +166,4 @@ module.exports = function (grunt) {
             })
         });
     })
-
-
 };
